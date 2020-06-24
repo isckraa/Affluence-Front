22f36085ff34a258ca4faef41211131f522d4c3d
Nelu <wf.nelu@yandex.ru>
2020-06-24 12:06:50 +0200

    review component Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,37 +8,32 @@ class Login extends React.Component {
         super(props);
     }
 
-    render() {
-        document.body.style.background = `url(${Backgound}) center center / cover no-repeat`;
-        document.body.style.height = '100vh';
-        document.body.style.display = 'flex';
-        document.body.style.justifyContent = 'space-around';
-        document.body.style.alignItems = 'center';
-        document.body.style.flexDirection = 'column';
-        
+    render() {        
         return(
             <Fragment>
-                <form className="login-form" method="POST">
-                    <h1 className="title">Affluence</h1>
-                    <div className="inputs">
-                        <div className="username-section input-section">
-                            <input type="text" name="username" className="input" autoComplete="off" required ref={(input) => this.input = input}/>
-                            <label for="username" class="label-name">
-                                <span class="content-name">Utilisateur</span>
-                            </label>
+                <div className="login-content" style={{background: `url(${Backgound}) center center / cover no-repeat`}}>
+                    <form className="login-form" method="POST">
+                        <h1 className="title">Affluence</h1>
+                        <div className="inputs">
+                            <div className="username-section input-section">
+                                <input type="text" name="username" className="input" autoComplete="off" required ref={(input) => this.input = input}/>
+                                <label for="username" class="label-name">
+                                    <span class="content-name">Utilisateur</span>
+                                </label>
+                            </div>
+                            <div className="password-section input-section">
+                                <input type="password" name="password" class="input" required/>
+                                <label for="password" class="label-name">
+                                    <span class="content-name">Mot de passe</span>
+                                </label>
+                            </div>
                         </div>
-                        <div className="password-section input-section">
-                            <input type="password" name="password" class="input" required/>
-                            <label for="password" class="label-name">
-                                <span class="content-name">Mot de passe</span>
-                            </label>
+                        <div className="login-form__submit-wrapper">
+                            <div className="login-form__button cta">Se Connecter</div>
+                            <div className="login-form__button cta">Créer un compte</div>
                         </div>
-                    </div>
-                    <div className="login-form__submit-wrapper">
-                        <div className="login-form__button cta">Se Connecter</div>
-                        <div className="login-form__button cta">Créer un compte</div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </Fragment>
         );
     }
