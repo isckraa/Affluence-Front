0d86f14082cf2582f72ff5c6a2ff9fc80375a8ed
nelu <wf.nelu@yandex.ru>
2020-06-25 19:21:27 +0200

    add page 404 and route system

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import NotFound from './components/NotFound';
+import Login from './components/Login';
 import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <SignUp/> */}
-        {/* <Login /> */}
-        <HomePage />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/style/NotFound.css';
+
+class NotFound extends React.Component {
+    render() {
+        return(
+            <div className="notfound-content">
+                <h1 className="notfound-title">404</h1>
+                <p className="notfound-text">Oops! Something is wrong.</p>
+                <a className="notfound-button" href="/">Go back in initial page, is better.</a>
+            </div>
+        )
+    }
+}
+
+export default NotFound;
